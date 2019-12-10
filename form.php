7709c68e137d25e9eff2408d3ed824d6cb435c34
dTum <50366412+dtumanenkov@users.noreply.github.com>
2019-12-10 15:54:33 +0300

    HW4

--- form.php
+++
@@ -1,6 +0,0 @@
-
-<form action="server.php" method="post" enctype="multipart/form-data">
-    <p>Выберите файл</p>
-    <input type="file" name="photo[]" multiple accept="image/*"><br>
-    <input type="submit" value="Сохранить">
-</form>
--- scan.php
+++
@@ -1,5 +0,0 @@
-<?php
-$files=scandir("images");
-foreach($files as $file){
-    echo"<img src='images/$file'><br>";
-}
